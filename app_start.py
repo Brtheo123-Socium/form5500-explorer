028f79fb6d505a5e7630a3fcd8f27b7df5e569d1
Ben Kotch <benkotch@MacBook-Air-261.localdomain>
2026-03-19 15:03:25 -0500

    fix db path hardcoded

--- app_start.py
+++ app_start.py
@@ -1,4 +1,7 @@
 import os
-os.environ.setdefault("DB_PATH", str(__import__('pathlib').Path.home() / "Form5500/output/form5500.db"))
+import sys
+os.environ['DB_PATH'] = '/data/form5500.db'
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 import app
-app.app.run(debug=False, port=int(os.environ.get("PORT", 8080)), host="0.0.0.0")
+app.DB = __import__('pathlib').Path('/data/form5500.db')
+app.app.run(debug=False, port=int(os.environ.get("PORT", 10000)), host="0.0.0.0")
